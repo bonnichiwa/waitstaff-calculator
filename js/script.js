82e6c056e9d51816c07424b8bcdfea235b5abab0
bonnichiwa <bonnie.pham@ryerson.ca>
2015-09-18 16:50:36 -0400

    can calculate basic subtotal and tips

---
+++ js/script.js
@@ -0,0 +1,15 @@
+angular.module('tipsCalculator', [])
+  .controller('tipsController', function($scope) {
+
+    // var defaultForm = {
+    //   name : "",
+    //   data : "",
+    //   task : "",
+    //   task2: "",
+    //   mentor: "",
+    //   skill: "",
+    //   adj: "",
+    //   celebrity: "",
+    //   number: ""
+    // }
+  });

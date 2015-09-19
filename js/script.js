8d86cda7c0ad1c26f19c0c05c266d3242c43519d
bonnichiwa <bonnie.pham@ryerson.ca>
2015-09-18 20:59:04 -0400

    added function for meal count, and placeholders for tip total and average tip

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
 angular.module('tipsCalculator', [])
   .controller('tipsController', function($scope) {
 
-    var defaultForm = {
+    var mealForm = {
       mealprice : "",
       taxpercent : "",
       tippercent : "",
     }
 
-    $scope.data = angular.copy(defaultForm);
+    $scope.data = angular.copy(mealForm);
 
     $scope.cancel = function() {
       $scope.tipsForm.$setPristine();
-      $scope.data = angular.copy(defaultForm);
+      $scope.data = angular.copy(mealForm);
       console.log('empty');
     }
+
+    $scope.submit = function() {
+      $scope.mealcount += 1;
+      console.log("meal count + 1");
+    }
+
+    $scope.tipTotal = 0;
+    $scope.mealCount = 0;
+    $scope.avgTip = 0;
 
   });

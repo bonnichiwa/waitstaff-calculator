0bb7c8bb90a75c4dbbdccd1f9eb436471f56b62c
bonnichiwa <bonnie.pham@ryerson.ca>
2015-09-18 20:29:29 -0400

    cancel button will clear form, and form starts off with -bash.00 values

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 angular.module('tipsCalculator', [])
   .controller('tipsController', function($scope) {
 
-    // var defaultForm = {
-    //   name : "",
-    //   data : "",
-    //   task : "",
-    //   task2: "",
-    //   mentor: "",
-    //   skill: "",
-    //   adj: "",
-    //   celebrity: "",
-    //   number: ""
-    // }
+    var defaultForm = {
+      mealprice : "",
+      taxpercent : "",
+      tippercent : "",
+    }
+
+    $scope.data = angular.copy(defaultForm);
+
+    $scope.cancel = function() {
+      $scope.tipsForm.$setPristine();
+      $scope.data = angular.copy(defaultForm);
+      console.log('empty');
+    }
+
   });

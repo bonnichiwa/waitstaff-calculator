9a94eab3afc42cf5675e686041ef978bbff616c4
bonnichiwa <bonnie.pham@ryerson.ca>
2015-09-20 11:34:16 -0400

    when users add a new entry now the form will reset to 0 values

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 angular.module('tipsCalculator', [])
   .controller('tipsController', function($scope) {
 
+    $scope.tipTotal = 0;
+    $scope.mealCount = 0;
+    $scope.avgTip = 0;
+
     var mealForm = {
       mealprice : "",
       taxpercent : "",
@@ -16,12 +20,17 @@ angular.module('tipsCalculator', [])
     }
 
     $scope.submit = function() {
-      $scope.mealcount += 1;
+      $scope.mealCount += 1;
       console.log("meal count + 1");
+      var addTip = (tipsForm.tippercent/100) * (tipsForm.mealprice + (tipsForm.mealprice * tipsForm.taxpercent/100));
+      $scope.tipTotal += addTip
+      console.log("tip total is: addTip");
+
+      $scope.tipsForm.$setPristine();
+      $scope.data = angular.copy(mealForm);
+      console.log('new entry');
     }
 
-    $scope.tipTotal = 0;
-    $scope.mealCount = 0;
-    $scope.avgTip = 0;
+
 
   });
